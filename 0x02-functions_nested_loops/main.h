f7f898eb2e79d1f3e27bbdf84513c279fa92e15a
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-02-21 14:00:11 -0800

    f8

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        char alp[8] = "_putchar";
+        int i;
+
+        for (i = 0; i < 8; i++)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
+}
