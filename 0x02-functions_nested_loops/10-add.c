a5f0d1040d7a5994c4fc4ac25b4dca4bdc49dbf6
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-02-22 11:12:15 -0800

    y6

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @a1 - first numbers to be computed
+ * @b2 - second number to be computed
+ *
+ * Return: integer value
+ */
+
+int add(int a1, int b2)
+{
+	return (a1 + b2);
+}
