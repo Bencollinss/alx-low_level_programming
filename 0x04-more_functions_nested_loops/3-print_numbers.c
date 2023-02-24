fba2438c2c72071a4385233b5fc2f41f52cd95bb
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-02-24 13:15:13 -0800

    kk

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,13 @@
 /**
  * print_numbers - Prints numbers from 0 to 9
  *
- * Return: Always 0.
+ * Return: The numbers since 0 up to 9
  */
 
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar(i + '0');
