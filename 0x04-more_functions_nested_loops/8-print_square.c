f8ded4f6a9fbd5133b7e3b867b0d25f936737542
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-02-24 14:35:10 -0800

    xo

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * main - Prints a Fizz Buzz program
+  *
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (1 % 5 == 0))
+		{
+		printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+		printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (x != 100);
+		{
+			printf(" ");
+		}
+	}
+}
