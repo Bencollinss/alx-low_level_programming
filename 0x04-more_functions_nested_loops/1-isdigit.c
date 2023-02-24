4653aa160183916a25730ab73dcd6d7260ddbea0
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-02-24 10:18:55 -0800

    zs

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @z: number to be checked 
+ *
+ * Return: 1 if character is digit and 0 otherwise
+ */
+
+int _isdigit(int c);
+{
+	if (z >= 48 && z <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
