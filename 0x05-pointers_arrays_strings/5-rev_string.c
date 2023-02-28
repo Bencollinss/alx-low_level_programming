039e262e80428a3634696e3b6136f15f206b63e8
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-02-27 19:51:28 -0800

    a function that prints half of a string, followed by a new line

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,34 +1,9 @@
 #include "main.h"
 
-/**
-  * rev_string - Reverses a string
-  * @s: The string to be modified
-  *
-  * Return: void
-  */
+/** A function that reverses a string
+ * @s: String to be modified
+ *
+ * Return: Void
+ */
 
-void rev_string(char *s)
-{
-	int i, c, k;
-	char *a, aux;
-
-	a = s;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
-}
+:
