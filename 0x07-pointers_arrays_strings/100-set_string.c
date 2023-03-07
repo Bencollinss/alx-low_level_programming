0f7e26e6e5f74c9bb9f638f46f2fccc86cab912e
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-03-07 14:14:15 -0800

    advanced tasks

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,4 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - a function that sets tha value o a char to a pointer
+ * @s: value tp modify
+ * @to: value to assign
+ *
+ * Return: Nothing
+ */
+
 void set_string(char **s, char *to)
 {
-    *s = to;
+	*s = to;
 }
