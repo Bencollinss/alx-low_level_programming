c61f5a5f54a828af763ca493665b9cf8e8edb93d
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-03-14 09:28:35 -0700

    update on task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,19 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int n1 = 0, n2 = 0;
+
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
-
-	printf("%d\n", result);
+	return (0);
 }
