df24819efb7e1377871d2e655a035891517c6580
Ben Mba Collins <mbakrnechukwucollins@gmail.com>
2023-03-31 03:22:55 -0700

    fixes some errors

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,6 @@
 #ifndef _LINKED_LIST
 #define _LINKED_LIST
 
-/* These are my header files
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -19,9 +12,9 @@
  */
 typedef struct list_s
 {
-        char *str;
-        unsigned int len;
-        struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _strlen(const char *s);
