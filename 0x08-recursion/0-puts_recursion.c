5d93d12cb9c3fe09010886e1c0d57f076860cf2a
Ben Collins <mbakrnechukwucollins@gmail.com>
2023-06-08 12:43:49 -0700

    A function that print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * main - a function that prints a string
+ * @s: The string
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return 0;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,8 @@
+# Recursion
+
+* A recursive function is a function that calls it self
+
+* There a many problems we can solve with recursion, we can solve the factorial of a number
+using recursion as follows.
+
+
