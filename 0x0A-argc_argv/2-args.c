95917d8efdccf311148d0c088538b501c86f9b98
Ben Collins <mbakrnechukwucollins@gmail.com>
2023-06-12 05:17:33 -0700

    prints al argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - A program that prints all argument passed to it
+ * @argc: Argument Count
+ * @argv: Argument Vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argc[x]);
+	}
+	return (0);
+}
