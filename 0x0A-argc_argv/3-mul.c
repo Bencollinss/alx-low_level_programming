4d90282ce01617188ce38e67f89827cc39727319
Ben Collins <mbakrnechukwucollins@gmail.com>
2023-06-12 06:10:17 -0700

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,27 @@
 #include "main.h"
 
 /**
+ * main - Multiplies two numbers
+ * @argc: Argument count
+ * @argv: Argument vector
  *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = 0, num2 = 0;
+
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]), num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
